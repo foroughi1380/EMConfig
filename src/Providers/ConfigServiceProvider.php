a21802d80dfef96cc75a4d4e57191040eebbcfd5
Mohammad Foroughi <foroughi1380@gmail.com>
2021-10-21 00:30:41 +0330

    bind EMConfig Class Config

--- src/Providers/ConfigServiceProvider.php
+++ src/Providers/ConfigServiceProvider.php
@@ -1,17 +1,20 @@
 <?php
-namespace Gelim\EMConfig;
+namespace Gelim\EMConfig\Providers;
 
+use Gelim\EMConfig\EMConfig;
 use Illuminate\Support\ServiceProvider;
 
 class ConfigServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-
+        dd("worked");
     }
 
     public function register()
     {
-
+        $this->app->singleton("emconfing-config" , function (){
+            return new EMConfig();
+        });
     }
 }
