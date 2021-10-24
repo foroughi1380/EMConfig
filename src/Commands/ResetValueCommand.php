f45510cc84c5885b8308cf0bcb616e098edaf961
Mohammad Foroughi <foroughi1380@gmail.com>
2021-10-24 19:35:04 +0330

    added reset value command file

---
+++ src/Commands/ResetValueCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Gelim\EMConfig\Commands;
+
+use Gelim\EMConfig\Facades\EMConfig;
+use Illuminate\Console\Command;
+
+class ResetValueCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'emconfig:resetValue {scope?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'set all config value to default value';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        EMConfig::resetValue($this->getArguments()['scope']??null);
+        return Command::SUCCESS;
+    }
+}
