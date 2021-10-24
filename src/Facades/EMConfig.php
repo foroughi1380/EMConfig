94fdef0a91ec6410ba189e39ddd792983dac8778
Mohammad Foroughi <foroughi1380@gmail.com>
2021-10-24 20:51:20 +0330

    write phpdoc

--- src/Facades/EMConfig.php
+++ src/Facades/EMConfig.php
@@ -1,6 +1,7 @@
 <?php
 namespace Gelim\EMConfig\Facades;
 
+use Gelim\EMConfig\Database\Models\Configuration;
 use Illuminate\Support\Facades\Facade;
 
 
@@ -9,6 +10,12 @@ use Illuminate\Support\Facades\Facade;
  * @method static void review()
  * @method static void resetValue($scope=null)
  * @method static \Gelim\EMConfig\EMConfig scope($scope)
+ * @method static array scopes()
+ * @method static array scopesKeysRaw($scope)
+ * @method static array keys($scope=null)
+ * @method static bool set($key, $value, $scope=null)
+ * @method static mixed get($key, $default=null,$scope=null)
+ * @method static Configuration|null getRow($key, $scope=null)
  */
 class EMConfig extends Facade
 {
