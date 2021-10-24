15a0a59e2775dabf34866de8ffbaac9160d37eaf
Mohammad Foroughi <foroughi1380@gmail.com>
2021-10-24 18:59:56 +0330

    add reset value php doc

--- src/Facades/EMConfig.php
+++ src/Facades/EMConfig.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @method static void init()
  * @method static void review()
+ * @method static void resetValue($scope=null)
  */
 class EMConfig extends Facade
 {
