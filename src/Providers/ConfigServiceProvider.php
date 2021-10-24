5a865bcb2db2fe93e62d3f2d979ba88472eb7374
Mohammad Foroughi <foroughi1380@gmail.com>
2021-10-24 15:11:51 +0330

    register IconfigRepository to EloquentConfigRepository

--- src/Providers/ConfigServiceProvider.php
+++ src/Providers/ConfigServiceProvider.php
@@ -1,6 +1,8 @@
 <?php
 namespace Gelim\EMConfig\Providers;
 
+use Gelim\EMConfig\Database\Repository\EloquentConfigRepository;
+use Gelim\EMConfig\Database\Repository\IConfigRepository;
 use Gelim\EMConfig\EMConfig;
 use Illuminate\Support\ServiceProvider;
 
@@ -8,13 +10,20 @@ class ConfigServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadMigrationsFrom("Database/Migrations");
+        $this->loadMigrationsFrom(__DIR__ . "/../Database/Migrations");
+        $this->publishes([
+            __DIR__ . "/../Configs/configSet.php" => config_path("configSet.php")
+        ],"emconfig");
     }
 
     public function register()
     {
         $this->app->singleton("emconfing-config" , function (){
             return new EMConfig();
+        });
+
+        $this->app->singleton(IConfigRepository::class , function (){
+            return new EloquentConfigRepository();
         });
     }
 }
