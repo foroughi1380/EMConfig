786edd82a57187493cedc413ecb7b616412aa034
Mohammad Foroughi <foroughi1380@gmail.com>
2021-10-24 15:11:19 +0330

    create init method

--- src/EMConfig.php
+++ src/EMConfig.php
@@ -1,6 +1,20 @@
 <?php
 namespace Gelim\EMConfig;
+use Gelim\EMConfig\Database\Repository\IConfigRepository;
+
+require_once ("Utilities.php");
 class EMConfig
 {
+    public function init()
+    {
+        /** @var IConfigRepository $repo */
+        $repo = resolve(IConfigRepository::class);
+        $rows = getDefaultConfigRow();
+        $repo->init($rows);
+    }
 
+    public function get($key, $default, $scope="default")
+    {
+
+    }
 }
