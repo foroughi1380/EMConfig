874187f00817708236ddb3f38da0dc7a6d723f22
MOhammad Foroughi <foroughi1380@gmail.com>
2021-11-07 13:34:37 +0330

    change flat file to class

--- src/Utilities.php
+++ src/Utilities.php
@@ -1,28 +1,29 @@
 <?php
 
 namespace Gelim\EMConfig;
-
-function getDefaultConfigRow($scope = null)
-{
-    $configs = config("configSet");
-    if ($scope){
-        $configs=[$scope => $configs[$scope]];
-    }
-    $ret = [];
-
-
-    foreach ($configs as $scope => $keys) {
-        foreach ($keys as $key => $value) {
-            $ret[] = [
-                "scope" => $scope,
-                "key" => $key,
-                "type" => $value['type'],
-                "extras" => $value['value'],
-                "title" => $value['title'],
-                "description" => $value['description'],
-            ];
+class Utilities{
+    function getDefaultConfigRow($scope = null)
+    {
+        $configs = config("configSet");
+        if ($scope){
+            $configs=[$scope => $configs[$scope]];
         }
-    }
+        $ret = [];
 
-    return $ret;
+
+        foreach ($configs as $scope => $keys) {
+            foreach ($keys as $key => $value) {
+                $ret[] = [
+                    "scope" => $scope,
+                    "key" => $key,
+                    "type" => $value['type'],
+                    "extras" => $value['value'],
+                    "title" => $value['title'],
+                    "description" => $value['description'],
+                ];
+            }
+        }
+
+        return $ret;
+    }
 }
