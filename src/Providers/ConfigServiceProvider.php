0986b0fc141c23bd7742d6f52d1045f4ed70b5f3
MOhammad Foroughi <foroughi1380@gmail.com>
2021-11-30 14:13:58 +0330

    register emconfig accessor

--- src/Providers/ConfigServiceProvider.php
+++ src/Providers/ConfigServiceProvider.php
@@ -1,12 +1,14 @@
 <?php
 namespace Gelim\EMConfig\Providers;
 
+use Gelim\EMConfig\Commands\AddCommand;
 use Gelim\EMConfig\Commands\InitCommand;
 use Gelim\EMConfig\Commands\ResetValueCommand;
 use Gelim\EMConfig\Commands\ReviewCommand;
 use Gelim\EMConfig\Database\Repository\EloquentConfigRepository;
 use Gelim\EMConfig\Database\Repository\IConfigRepository;
 use Gelim\EMConfig\EMConfig;
+use Gelim\EMConfig\EMConfigSetManager;
 use Illuminate\Support\ServiceProvider;
 
 class ConfigServiceProvider extends ServiceProvider
@@ -15,12 +17,13 @@ class ConfigServiceProvider extends ServiceProvider
     {
         $this->loadMigrationsFrom(__DIR__ . "/../Database/Migrations");
         $this->publishes([
-            __DIR__ . "/../Configs/configSet.php" => config_path("configSet.php")
+            __DIR__ . "/../Configs/". EMConfig::CONFIG_FILE_NAME . ".php"=> config_path(EMConfig::CONFIG_FILE_NAME . ".php")
         ],["emconfig", "laravel-assets"]);
         $this->commands([
             InitCommand::class,
             ReviewCommand::class,
-            ResetValueCommand::class
+            ResetValueCommand::class,
+            AddCommand::class
         ]);
     }
 
@@ -33,5 +36,11 @@ class ConfigServiceProvider extends ServiceProvider
         $this->app->singleton(IConfigRepository::class , function (){
             return new EloquentConfigRepository();
         });
+
+        if ($this->app->runningInConsole()){
+            $this->app->singleton("emconfig-configset-manager", function (){
+                return new EMConfigSetManager();
+            });
+        }
     }
 }
