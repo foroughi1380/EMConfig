a8968d5e96dc551f45d782f2922237ce78f73696
Mohammad Foroughi <foroughi1380@gmail.com>
2022-02-09 05:12:08 +0330

    add: new ORM class mapping

---
+++ src/Database/Models/Config.php
@@ -0,0 +1,51 @@
+<?php
+namespace Gelim\EMConfig\Database\Models;
+use Gelim\EMConfig\Casts\SerialCast;
+use Illuminate\Database\Eloquent\Model;
+
+class Configuration extends Model
+{
+    protected $table = "emconfig";
+    protected $fillable = [
+        'scope',
+        'key',
+        'value',
+        'type',
+        'extras'
+    ];
+
+    protected $casts = [
+        "value" => ""
+    ];
+
+    /**
+     * @return string[]
+     */
+    public function getCasts(): array
+    {
+        $casts = parent::getCasts();
+
+        if (empty($this->type)) return $casts;
+
+        switch (strtolower($this->type)) {
+            case "array":
+            case "any":
+                $valueCast = SerialCast::class;
+                break;
+            case "multiline":
+                $valueCast = "string";
+                break;
+            default:
+                $valueCast = $this->type;
+        }
+
+        $casts['value'] = $valueCast??$casts['value'];
+        return $casts;
+    }
+
+
+    public function scopeScope($query, $scope)
+    {
+        return $query->where("scope" , $scope);
+    }
+}
